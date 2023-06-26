1751b4a7b233567b46dd99aaa960719aa3f429c6
Jair <jairaceves56@gmail.com>
2023-06-26 08:08:38 -0600

    Command Color Pallet Added

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      primaryColor: '#3195ff',
+      secondaryColor:' #333',
+      darkColor: '#000',
+      lightColor: '#f4f4f4',
+      greenBorder: '#1CB698',
+      veryDarkBlue: 'hsl(229, 31%, 21%)',
+      yellowSpan: '#ceaa0d',
+      coverColor: '#2b6a7ef7',
+      buttonsColor: '#133B5C',
+      buttonColor: '#1D2D50',
+      greenColor: '#03C988',
+      beigeColor: '#FCDAB7',
+      redColor: '#B42B51',
+      grayColor: '#BDCDD6',
+      skyblueColor: '#6096B4',
+    },
+    fontFamily: {
+      sans: ['Barlow Semi Condensed', 'sans-serif'],
+      mono: ['Righteous'],
+    },
+    opacity: {
+      '8': '.8',
+    }
+  },
+  plugins: [],
+}
