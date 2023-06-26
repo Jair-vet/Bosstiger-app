e1c2a58ef67694a8a6a776f72d88eb66a3f6e7e4
Jair <jairaceves56@gmail.com>
2023-06-26 11:28:18 -0600

    Created a Home Page Screen

--- frontend/src/screens/HomePage.jsx
+++ frontend/src/screens/HomePage.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import products from '../products'
+import { Product } from '../components/Product'
+
 
 export const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className='p-2 container'>
+        <h1 className='text-center text-2xl uppercase font-sans font-bold'>Lastest <span>Products</span></h1>
+        <div className='p-8 grid gap-5 xl:grid-cols-6 lg:grid-cols-4 md:grid-cols-3 grid-cols-2'>
+            {products.map((product) => (
+                <Product
+                    product={product}
+                    key={product._id}
+                />
+            ))}
+        </div>
+
+        {/* Catalogo */}
+        {/* <Catalogo /> */}
+    </div>
   )
 }
