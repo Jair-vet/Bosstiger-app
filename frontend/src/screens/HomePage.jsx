68ade73461828a10d3f6f6386f04627e27b75517
Jair <jairaceves56@gmail.com>
2023-06-27 21:06:54 -0600

    We apply the Product Slice

--- frontend/src/screens/HomePage.jsx
+++ frontend/src/screens/HomePage.jsx
@@ -1,37 +1,42 @@
-import React, { useEffect, useState } from 'react'
 import { Product } from '../components/Product'
-import axios from 'axios'
-
+import { useGetProductsQuery } from '../slices/productsApiSlice'
 
 export const HomePage = () => {
-  
-  const [products, setProducts] = useState([])
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products')
-
-      setProducts(data)
-    }
-
-    fetchProducts()
-  }, [])
-
+  const { data: products, isLoading, error } = useGetProductsQuery()
 
   return (
-    <div className='p-2 container md:pt-26 pt-36'>
-        <h1 className='text-center text-2xl uppercase font-sans font-bold'>Lastest <span>Products</span></h1>
-        <div className='p-8 grid gap-5 lg:grid-cols-4 md:grid-cols-3 grid-cols-2'>
-            {products.map((product) => (
-                <Product
-                    product={product}
-                    key={product._id}
-                />
-            ))}
-        </div>
+    <>
+      { 
+        isLoading 
+          ? 
+            (<h2>Loading...</h2>) 
+          : error 
+          ? 
+            (<div className='h-screen items-center w-full justify-center'>
+              <p className='text-3xl text-darkColor'>
+                { error?.data.message || error.error }
+              </p>
+            </div>) 
+          : 
+            (<>
+              <div className='p-2 container md:pt-26 pt-36'>
+                  <h1 className='text-center text-2xl uppercase font-sans font-bold'>Lastest <span>Products</span></h1>
+                  <div className='p-8 grid gap-5 lg:grid-cols-4 md:grid-cols-3 grid-cols-2'>
+                      {products.map((product) => (
+                          <Product
+                              product={product}
+                              key={product._id}
+                          />
+                      ))}
+                  </div>
 
-        {/* Catalogo */}
-        {/* <Catalogo /> */}
-    </div>
+                  {/* Catalogo */}
+                  {/* <Catalogo /> */}
+              </div>
+            </>) 
+        }
+      
+    </>
   )
 }
