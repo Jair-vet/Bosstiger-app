90493e7388a4064628a53a08191af6aa19d71623
Jair <jairaceves56@gmail.com>
2023-07-05 15:59:52 -0600

    Create a Component to extract the personal info

--- frontend/src/components/Dropdown.jsx
+++ frontend/src/components/Dropdown.jsx
@@ -1,12 +1,35 @@
 import { Fragment } from 'react'
 import { Menu, Transition } from '@headlessui/react'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { useLogoutMutation } from '../slices/usersApiSlice';
+import { logout } from '../slices/authSlice';
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
 export const Dropdown = ({user}) => {
+    
+    const { cartItems } = useSelector((state) => state.cart);
+    const { userInfo } = useSelector((state) => state.auth);
+    
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    
+    const [logoutApiCall] = useLogoutMutation();
+
+    const logoutHandler = async () => {
+        try {
+          await logoutApiCall().unwrap();
+          dispatch(logout());
+          navigate('/login');
+        } catch (err) {
+          console.error(err);
+        }
+    };
+
   return (
     <Menu as="div" className="relative inline-block md:text-left text-center">
         <div>
@@ -42,21 +65,20 @@ export const Dropdown = ({user}) => {
                 </Menu.Item>
                 
                 
-                <form method="POST" action="#">
-                    <Menu.Item>
-                    {({ active }) => (
-                        <a
-                            type="submit"
-                            className={classNames(
-                                active ? 'text-primaryColor' : 'text-secondaryColor',
-                                'block w-full px-4 py-2 md:text-left text-center text-sm uppercase hover:text-lightColor'
-                            )}
-                        >
-                        Sign out
-                        </a>
-                    )}
-                    </Menu.Item>
-                </form>
+                <Menu.Item>
+                {({ active }) => (
+                    <button
+                        type="submit"
+                        className={classNames(
+                            active ? 'text-primaryColor' : 'text-secondaryColor',
+                            'block w-full px-4 py-2 md:text-left text-center text-sm uppercase hover:text-lightColor'
+                        )}
+                        onClick={logoutHandler}
+                    >
+                    Sign out
+                    </button>
+                )}
+                </Menu.Item>
             </div>
         </Menu.Items>
         </Transition>
