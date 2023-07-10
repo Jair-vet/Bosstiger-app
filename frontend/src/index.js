69761005962db48bae2c08e304e9c9e7ef7ba811
Jair <jairaceves56@gmail.com>
2023-07-10 17:53:52 -0600

    Create the paypal root

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,6 +14,7 @@ import { ShippingScreen } from './screens/ShippingScreen';
 import { PaymentScreen } from './screens/PaymentScreen';
 import { PlaceOrderScreen } from './screens/PlaceOrderScreen';
 import { OrderScreen } from './screens/OrderScreen';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 
 
 const router = createBrowserRouter([
@@ -62,7 +63,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <PayPalScriptProvider deferLoading={true}>
+        <RouterProvider router={router} />
+      </PayPalScriptProvider>
     </Provider>
   </React.StrictMode>
 );
