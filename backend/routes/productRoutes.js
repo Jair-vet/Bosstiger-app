536be38ef072883ffda82cf416090fa8ed65d088
Jair <jairaceves56@gmail.com>
2023-07-11 17:42:44 -0600

    Create the put and delete method in the routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,12 +2,18 @@ import express from 'express';
 const router = express.Router();
 import {  createProduct, 
           getProducts, 
-          getProductsById 
+          getProductsById, 
+          updateProduct
 } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js'
+import checkObjectId from '../middleware/checkObjectId.js';
 
 
 router.route('/').get(getProducts).post(protect, admin, createProduct)
-router.route('/:id').get(getProductsById)
+router
+    .route('/:id')
+    .get(checkObjectId, getProductsById)
+    .put(protect, admin, checkObjectId, updateProduct)
+    .delete(protect, admin, checkObjectId, deleteProduct);
 
 export default router;
