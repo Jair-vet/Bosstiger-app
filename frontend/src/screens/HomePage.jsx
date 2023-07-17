74b9482074800f24c9dc1daffc1cbc9fe48e1dc3
Jair <jairaceves56@gmail.com>
2023-07-17 16:04:18 -0600

    Create the paginate backend

--- frontend/src/screens/HomePage.jsx
+++ frontend/src/screens/HomePage.jsx
@@ -1,9 +1,11 @@
 import { Product } from '../components/Product'
+import { useParams } from 'react-router-dom'
 import { useGetProductsQuery } from '../slices/productsApiSlice'
 
 export const HomePage = () => {
 
-  const { data: products, isLoading, error } = useGetProductsQuery()
+  const { pageNumber } = useParams()
+  const { data, isLoading, error } = useGetProductsQuery({ pageNumber })
 
   return (
     <>
@@ -29,7 +31,7 @@ export const HomePage = () => {
               <div className='p-2 container md:pt-26 pt-36'>
                   <h1 className='text-center text-2xl uppercase font-sans font-bold'>Lastest <span>Products</span></h1>
                   <div className='p-8 grid gap-5 lg:grid-cols-4 md:grid-cols-3 grid-cols-2'>
-                      {products.map((product) => (
+                      {data.products.map((product) => (
                           <Product
                               product={product}
                               key={product._id}
--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -4,9 +4,9 @@ import { apiSlice } from './apiSlice';
 export const productsApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getProducts: builder.query({
-      query: () => ({
+      query: ({ pageNumber }) => ({
         url: PRODUCTS_URL,
-        // params: { keyword, pageNumber },
+        params: { /* keyword, */ pageNumber },
       }),
       keepUnusedDataFor: 5,
       providesTags: ['Products'],
