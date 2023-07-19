dbf6077075a28ef2dd5f248cfb1ac863542cbd07
Jair <jairaceves56@gmail.com>
2023-07-18 18:00:59 -0600

    We update the app to create the build on Production

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -7,7 +7,7 @@ import {
     updateProduct,
     deleteProduct,
     createProductReview,
-    // getTopProducts,
+    getTopProducts,
 } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js'
 import checkObjectId from '../middleware/checkObjectId.js';
@@ -15,7 +15,7 @@ import checkObjectId from '../middleware/checkObjectId.js';
 
 router.route('/').get(getProducts).post(protect, admin, createProduct);
 router.route('/:id/reviews').post(protect, checkObjectId, createProductReview);
-// router.get('/top', getTopProducts);
+router.get('/top', getTopProducts);
 router
   .route('/:id')
   .get(checkObjectId, getProductsById)
